1ef56b096c000c9d24a845f04434482631ae357d
tomo <37551277+nondescryptid@users.noreply.github.com>
2023-01-09 15:14:43 +0800

    Create citations.py

---
+++ citations.py
@@ -0,0 +1,4 @@
+# Accept a citation and save it as a dictionary as such:
+# McGill: <PARTY NAME>, <YEAR> <COURT> <CASE>. e.g. Radonna Investments Ltd v Rubin, 2012 ONCA 321.                                                                                                                                                                                                                                         
+# OSCOLA: <PARTY NAME> <[YEAR]> <COURT> <CASE>. e.g. Corr v IBC Vehicles Ltd [2008] UKHL 13
+
