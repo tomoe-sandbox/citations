ce66b0d2811f8b2bea59d06b53c95f5aebd721c2
nondescryptid <suzukitomoe.st@gmail.com>
2023-01-31 10:36:03 +0800

    updated test

--- citations.py
+++ citations.py
@@ -1,35 +1,35 @@
+# def main():
+casenames = []
+citations = []
 
-# Accept a citation and save it as a dictionary as such:
+active = True
+while active:
+    user_input = input("Please input your complete case citation here, Quit to exit: ")
+    # if input is Quit, then exit
+    if user_input == 'Quit':
+        print(casenames)
+        print(citations)
+        active = False
+        break
+    else:
+        # get citations
+        for i in range(len(user_input)):
+            if (i+1)<len(user_input) and user_input[i+1] == '[':
+                casename = user_input[:i]
+                cite= user_input[i+1:]
+                casenames.append(casename)
+                citations.append(cite)
+                print(casename + ', ' + cite)
+# user_input = input("Please input your complete case citation here, Quit to exit: ")
+# for i in range(len(user_input)):
+#             if (i+1)<len(user_input) and user_input[i+1] == '[':
+#                 casename = user_input[:i]
+#                 cite= user_input[i+1:]
+#                 print(casename + ', ' + cite)
 
-# McGill: <PARTY NAME>, <YEAR> <COURT> <CASE>. e.g. Radonna Investments Ltd v Rubin, 2012 ONCA 321.                                                                                                                                                                                                                                         
-# OSCOLA: <PARTY NAME> <[YEAR]> <COURT> <CASE>. e.g. Corr v IBC Vehicles Ltd [2008] UKHL 13; Attorney-General v Ting Choon Meng [2017] SGCA 6 
 
-# # We've created a dictionary called "citation" for you.
-# citation = {
-#     "party": "",
-#     "year": "",
-#     "court":"",
-#     "case_no": ""
-# }
+# #  Don't touch this
+# if __name__ == "__main__":
+#     main()
 
-# # Create list for recording users' citations 
-# citations = []
-
-
-# # Accept user input
-# is_active = True
-
-# while is_active:
-#     user_cite = input("Enter your citation here in the format PARTY YEAR COURT CASE e.g. Corr v IBC Vehicles Ltd 2008 UKHL 13: ")
-#     # We want to get everything before the year.
-#     index = 0 
-#     # Get party name 
-#     for char in user_cite:
-#         if char.isdigit():
-#             index = user_cite.index(char)
-#             party = user_cite[0:index]
-#             citation["party"] = party
-#             print(citation)
-#             break
-print("Hello!")
-
+# If I put in CASE1, CASE2 and Quit, it should give me an output of...
--- citations_test.py
+++ citations_test.py
@@ -1,19 +1,105 @@
-from io import StringIO
-import subprocess
-import citations
+import builtins
+import importlib
+import io
+import sys
+# def test_PA0(monkeypatch, capfd):
+#     PA0.main()
+#     monkeypatch.setattr('builtins.input', lambda _: "A v B [2020] 3 SLR 322")
+#     monkeypatch.setattr('builtins.input', lambda _: "Quit")
+#     out, err = capfd.readouterr()
+#     assert out == "A v B [2020] 3 SLR 322"
+# https://stackoverflow.com/questions/73354667/how-to-test-a-single-python-file-having-no-functions-in-it-how-to-test-for-mult
 
-# case = StringIO("Corr v IBC Vehicles Ltd 2008 UKHL 13\n")
+# Testing loops where input must be repeatedly entered: Use a generator https://stackoverflow.com/questions/70597270/pytest-not-looping-while-testing-with-monkeypatching
 
-# def test_year(monkeypatch, capsys):
-#     subprocess.run(["python", "citations.py"], stdout=subprocess.PIPE, check=True)
-#     monkeypatch.setattr('sys.stdin', case)
-#     captured = capsys.readouterr()
-#     assert captured.out == {'party': 'Corr v IBC Vehicles Ltd ', 'year': '', 'court': '', 'case_no': ''}
+# helper functions
+def make_mock_input(generator):
+    def mock_input(*args, **kwargs): # ignore any arguments
+        return next(generator) # the state of the generator is stored in the enclosing scope of make_mock_input
+    return mock_input
 
-    # https://stackoverflow.com/questions/73354667/how-to-test-a-single-python-file-having-no-functions-in-it-how-to-test-for-mult 
-def test_hi(capsys):
-    subprocess.run(["python", "citations.py"], stdout=subprocess.PIPE, check=True)
-    captured = capsys.readouterr()
-    assert captured.out == "Hello!\n"
+def input_generator(user_input):
+        yield from user_input
 
-# https://stackoverflow.com/questions/73354667/how-to-test-a-single-python-file-having-no-functions-in-it-how-to-test-for-mult 
+# tests
+# https://code-maven.com/slides/python/pytest-mock-stdin 
+#  We don't use monkeypatch because we need the original input prompt, as opposedto the one that monkeypatch mocks.
+def test_input_exists(capsys):
+    '''
+    1. Checks that input exists and has the prompt "Please input your complete case citation here, Quit to exit: "
+    '''
+    user_input = ("\n", "Quit")
+    gen = input_generator(user_input)
+    mock_input = make_mock_input(gen)
+    sys.modules.pop('PA0', None)
+    sys.stdin = io.StringIO("\n" + "Quit") # lmao
+    import PA0
+    out, err = capsys.readouterr()
+
+    assert "Please input your complete case citation here, Quit to exit: " * 2 in out # Note that prompt will be asked twice (newline, and Quit)
+    
+def test_input_parse(monkeypatch, capsys):
+    user_input = ("A v B [2020] 3 SLR 322", "Quit")
+    gen = input_generator(user_input) # create the generator from your input values
+    mock_input = make_mock_input(gen)
+    monkeypatch.setattr("builtins.input", mock_input)
+    sys.modules.pop('PA0', None)
+    import PA0
+    out, err = capsys.readouterr()
+    assert "A v B, [2020] 3 SLR 322\n" in out # We use this instead of checking for equality as there may be other outputs
+    # https://www.freecodecamp.org/news/python-new-line-and-how-to-python-print-without-a-newline/ - Python adds a new line character at the end of a string as the default value of the end parameter of print() is \n.
+
+
+def test_citation_array(monkeypatch, capsys):
+    
+    user_input = ("A v B [2020] 3 SLR 322", "B v C [2019] 4 SLR 344", "Quit")
+    gen = input_generator(user_input) # create the generator from your input values
+    mock_input = make_mock_input(gen)
+    monkeypatch.setattr("builtins.input", mock_input)
+    sys.modules.pop('PA0', None)
+    import PA0
+    out, err = capsys.readouterr()
+    assert out == "A v B, [2020] 3 SLR 322\nB v C, [2019] 4 SLR 344\n['A v B', 'B v C']\n['[2020] 3 SLR 322', '[2019] 4 SLR 344']\n" # We use this instead of checking for equality as there may be other outputs
+    # https://www.freecodecamp.org/news/python-new-line-and-how-to-python-print-without-a-newline/ - Python adds a new line character at the end of a string as the default value of the end parameter of print() is \n.
+
+
+
+# def test_input_parsed(capsys, monkeypatch):
+#     ''''''
+#     mocked_stdout = io.StringIO()
+#     with monkeypatch.context() as m:
+#         m.setattr(builtins, "input", lambda prompt="": "A v B [2020] 3 SLR 322")
+
+#         m.setattr(sys, "stdout", mocked_stdout)
+   
+#         sys.modules.pop('PA0', None)
+#         importlib.import_module(name="PA0", package="citations")
+#         m.setattr(builtins, "input", lambda prompt="": "Quit") 
+#     assert mocked_stdout.getvalue().strip() == "A v B, [2020] 3 SLR 322"
+
+
+'''
+
+Notes:
+
+test_PA0 works when it is a straightforward 
+    user_input = input("Please input your complete case citation here, Quit to exit: ")
+    print(user_input*10)
+situation.
+Perhaps the problem lies with loops and the need to accept input -> print -> wait for multiple input
+
+Next steps: Redirect stdout to another file and then 
+'''
+    
+
+'''
+Test cases
+1. Check that a program asks for an input case citation with the following point: Please input your complete case citation here, Quit to exit
+2. Accepts a case citation as input. No validation needed. (Pf v Df [Year] Vol CitatorName Page)
+3. Separate case name from the case citation and print the result, separated by a comma
+4. Case name and case citation are saved in 2 separate lists. 
+5. Have the program repeat steps 1 to 4 to ask for and process input case citations repetitively. one at a time, until the user types Quit.
+6. On exit, print out lists of case names and case citations. 
+
+
+'''
